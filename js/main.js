ee722136a265a90dfde4dbdb9479978e60aeabb2
Lea <lea.senn@hotmail.de>
2017-05-03 11:03:37 +0200

    CSS Anpassung Audio, Danksagung, Google Analytics einschalten

--- js/main.js
+++ js/main.js
@@ -603,7 +603,7 @@ function grossschreibung(str) {
 //********************************************
 // Google Analytics 
 //********************************************
-/*
+
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -613,6 +613,6 @@ function grossschreibung(str) {
   ga('send', 'pageview');
 
 
-*/
+
 
 
